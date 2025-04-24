e797543f0b70ad0195f3d6caf5ed1d42cc510045
hurleyvalen <hurleyvalen.work@gmail.com>
2025-04-24 16:54:05 +0800

    setup react
    -empty components

---
+++ thrift-throat/src/components/ListGroup.tsx
@@ -0,0 +1,4 @@
+//TEMPLATE FOR COMPONENT FILE
+function ListGroup() {
+  return <h1>List Group</h1>;
+}
---
+++ thrift-throat/src/components/sections/Home.tsx
@@ -0,0 +1,7 @@
+// THIS IS FOR THE LANDING PAGE OR HOME PAGE
+
+function Home() {
+  return <h1>Hello World</h1>;
+}
+
+export default Home;
