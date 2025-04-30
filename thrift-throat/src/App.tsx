b9035f961ac34051092735a4bb0e6334eba53554
hurleyvalen <hurleyvalen.work@gmail.com>
2025-04-30 16:44:54 +0800

    Revert "remove everything then put backup"
    This reverts commit e045457e467eaafde71462c11175b039da367e32.

---
+++ thrift-throat/src/App.tsx
@@ -0,0 +1,43 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/sections/Home";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import ProductPage from "./components/sections/ProductPage";
+import FaqsPage from "./components/sections/FaqsPage";
+import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
+import LoadingPage from "./components/LoadingPage";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2100);
+  }, []);
+
+  useEffect(() => {
+    const body = document.body;
+    if (!loading) body.classList.add("app-ready");
+    else body.classList.remove("app-ready");
+  }, [loading]);
+
+  return (
+    <BrowserRouter>
+      <div className="main">
+        <Navbar />
+        <Routes>
+          <Route path="/ThrifthroatReact" element={<Home />} />
+          <Route path="/ThrifthroatReact/products" element={<ProductPage />} />
+          <Route path="/ThrifthroatReact/faq" element={<FaqsPage />} />
+        </Routes>
+      </div>
+      <Footer />
+      <LoadingPage />
+    </BrowserRouter>
+  );
+}
+
+export default App;
